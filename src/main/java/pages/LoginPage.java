80ba84563a307fee56396c7d0117c37fe7535b7a
Smitha Geetha <smitha.geetha@digital.cabinet-office.gov.uk>
2026-09-04 13:20:09 +0100

    Clean up LoginPage.java by removing comments
    Removed commented instructions for locating elements and methods.

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -6,15 +6,11 @@ import org.openqa.selenium.By;
 
 public class LoginPage {
 
-/* 1. Locate and find the below elements from LoginPage:
+
     accessibility ID "username"  ---> Input Box
     accessibility ID "password" -----> Input Box
     accessibility ID "loginButton" ---> Command button*/
 
-    // 2.Create a method "enterUsername" to enter username value to the username field
-
-    // 3.Create a method "enterPassword" to enter password value to the password field
-
-    //4. Create a method "clickLogin" to click on loginButton
+   
 
 }
