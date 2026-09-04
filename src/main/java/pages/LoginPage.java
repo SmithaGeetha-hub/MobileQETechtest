3b7c72d29c8cf4433e4dfd1214e5428f9d3094f1
Smitha Geetha <smitha.geetha@digital.cabinet-office.gov.uk>
2026-09-04 13:20:30 +0100

    Document accessibility IDs in LoginPage
    Added comments for accessibility IDs of login elements.

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 
 public class LoginPage {
 
-
+/*
     accessibility ID "username"  ---> Input Box
     accessibility ID "password" -----> Input Box
     accessibility ID "loginButton" ---> Command button*/
