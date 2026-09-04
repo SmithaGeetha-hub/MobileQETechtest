d7f498d2d99d62f1f761e1515259f3087ba86d22
Smitha Geetha <smitha.geetha@digital.cabinet-office.gov.uk>
2026-09-04 13:22:48 +0100

    Remove placeholder comments in LoginTest
    Removed placeholder comments for driver initialization, successful login test, and driver closure.

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -13,20 +13,17 @@ public class LoginTest {
     @BeforeMethod
     @Parameters("platform")
     public void setUp(String platform) throws Exception {
-        // write  code to initialise the driver  according to the chosen platform
+        
     }
 
     @Test
     public void successfulLogin() {
 
-        // write a successful login test for mobile app login scenario by :
-        //1. Entering username and password in the LoginPage and click the Login Button
-        //2. Entering OtpInput in the OtpPage and click verify
-        //3. Finally verify that the HomePage with home label is displayed , if not fail the test with the message "Home Page is not displayed".
+        
     }
 
     @AfterMethod
     public void tearDown() {
-        // write  code to close all driver instances
+       
     }
 }
