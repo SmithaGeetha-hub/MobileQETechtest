394a61f991ddc9a47f40a07d184c535815ef83eb
Smitha Geetha <smitha.geetha@digital.cabinet-office.gov.uk>
2026-09-04 13:21:08 +0100

    Clean up comments in OtpPage.java
    Removed commented instructions for locating elements and methods.

--- src/main/java/pages/OtpPage.java
+++ src/main/java/pages/OtpPage.java
@@ -10,11 +10,9 @@ import java.time.Duration;
 
 public class OtpPage {
 
-   /* 1. Locate and find the below elements from OtpPage:
+ /*
      accessibility ID "otpInput"  ---> Input/text Box
      accessibility ID "verifyButton" ---> Command button*/
 
-    // 2.Create a method "enterOtp" to enter otpInput value to the otpInput field
-
-    //3. Create a method "clickVerify" to verify the  entered otpInput value
+    
 }
